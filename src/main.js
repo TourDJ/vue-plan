50f65aa29844f8f7bc11c9865c21c095d29c79c0
smallbucket <943744213@qq.com>
2019-11-27 12:02:58 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
